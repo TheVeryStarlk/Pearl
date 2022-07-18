bfe9f0fd222ca1b2a5a3b63267b4b611c0e6b29d
starlkytminecraft@gmail.com <starlkytminecraft@gmail.com>
2022-07-19 01:19:06 +0300

    Forgot to add the `DbSet` in the database context.

--- Pearl.Database/PearlContext.cs
+++ Pearl.Database/PearlContext.cs
@@ -7,6 +7,8 @@ public sealed class PearlContext : DbContext
 {
     public DbSet<User> Users { get; set; } = null!;
 
+    public DbSet<RefreshToken> RefreshTokens { get; set; } = null!;
+
     public PearlContext(DbContextOptions<PearlContext> options) : base(options)
     {
     }
