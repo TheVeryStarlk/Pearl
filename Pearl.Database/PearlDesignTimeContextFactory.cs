d831d388dd6ec1d79ad41deb8379bbfcedc437ed
starlkytminecraft@gmail.com <starlkytminecraft@gmail.com>
2022-07-21 18:22:17 +0300

    Create a design-time context factory.

---
+++ Pearl.Database/PearlDesignTimeContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Pearl.Database;
+
+public sealed class PearlDesignTimeContextFactory : IDesignTimeDbContextFactory<PearlContext>
+{
+    public PearlContext CreateDbContext(string[] args)
+    {
+        var builder = new DbContextOptionsBuilder<PearlContext>();
+        builder.UseNpgsql("Host=localhost;Database=pearl;Username=postgres;Password=123");
+
+        return new PearlContext(builder.Options);
+    }
+}
