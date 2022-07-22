c67b312861dc9ea65e808976f7d144933613babe
starlkytminecraft@gmail.com <starlkytminecraft@gmail.com>
2022-07-22 22:02:29 +0300

    Add a table for groups and did a migration for it.

---
+++ Pearl.Database/Models/Group.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pearl.Database.Models;
+
+public sealed class Group
+{
+    [Required]
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; } = null!;
+}
