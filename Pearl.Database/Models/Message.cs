3038ed122fedeb79a82bb61578f436debdea9df4
starlkytminecraft@gmail.com <starlkytminecraft@gmail.com>
2022-07-25 04:33:19 +0300

    Add a table for messages and did a migration for it.

---
+++ Pearl.Database/Models/Message.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pearl.Database.Models;
+
+public sealed class Message
+{
+    [Required]
+    public int Id { get; set; }
+
+    [Required]
+    public string Content { get; set; } = null!;
+
+    [Required]
+    public Group Group { get; set; } = null!;
+
+    [Required]
+    public User User { get; set; } = null!;
+}
--- Pearl.Database/PearlContext.cs
+++ Pearl.Database/PearlContext.cs
@@ -7,6 +7,8 @@ public sealed class PearlContext : DbContext
 {
     public DbSet<Group> Groups { get; set; } = null!;
 
+    public DbSet<Message> Messages { get; set; } = null!;
+
     public DbSet<RefreshToken> RefreshTokens { get; set; } = null!;
 
     public DbSet<User> Users { get; set; } = null!;
