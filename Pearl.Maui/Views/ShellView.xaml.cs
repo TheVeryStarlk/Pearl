0d95b7abc523126fb807397cfe89254872f70300
starlkytminecraft@gmail.com <starlkytminecraft@gmail.com>
2022-07-27 22:34:01 +0300

    Automatically navigate to the groups view if the client is already authenticated.

--- Pearl.Maui/Views/ShellView.xaml.cs
+++ Pearl.Maui/Views/ShellView.xaml.cs
@@ -1,9 +1,54 @@
-﻿namespace Pearl.Maui.Views;
+﻿using CommunityToolkit.Mvvm.Messaging;
+using Pearl.Maui.Messages;
+using Pearl.Maui.Services;
+
+namespace Pearl.Maui.Views;
 
 public sealed partial class ShellView : Shell
 {
-    public ShellView()
+    private bool backButtonRequested;
+
+    private readonly AuthenticationService authenticationService;
+
+    public ShellView(AuthenticationService authenticationService)
     {
         InitializeComponent();
+        this.authenticationService = authenticationService;
+    }
+
+    protected override bool OnBackButtonPressed()
+    {
+        backButtonRequested = base.OnBackButtonPressed();
+        return backButtonRequested;
+    }
+
+    protected override async void OnNavigated(ShellNavigatedEventArgs args)
+    {
+        if (backButtonRequested)
+        {
+            base.OnNavigated(args);
+            return;
+        }
+
+        if (args.Current.Location.OriginalString.Contains(nameof(GroupsView)))
+        {
+            return;
+        }
+
+        var accessToken = Preferences.Get("AccessToken", null);
+        if (accessToken is null)
+        {
+            return;
+        }
+
+        var response = await authenticationService.RefreshAsync(accessToken);
+
+        if (response.IsSuccess)
+        {
+            await Current.GoToAsync(nameof(GroupsView));
+            WeakReferenceMessenger.Default.Send<GroupsMessage>();
+        }
+
+        base.OnNavigated(args);
     }
 }
